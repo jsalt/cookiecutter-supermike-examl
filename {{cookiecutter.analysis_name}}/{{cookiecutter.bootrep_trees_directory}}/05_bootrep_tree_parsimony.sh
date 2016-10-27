3d18ae2ab2d30ef712c869e7aae715b7ac711685
Carl Oliveros <carloliveros@gmail.com>
2016-10-27 14:32:10 -0500

    Update 05_bootrep_tree_parsimony.sh

--- {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/05_bootrep_tree_parsimony.sh
+++ {{cookiecutter.analysis_name}}/{{cookiecutter.bootrep_trees_directory}}/05_bootrep_tree_parsimony.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q single
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=5:00:00
+#PBS -l walltime=12:00:00
 #PBS -o 05_bootrep_tree_parsimonator.stdout
 #PBS -e 05_bootrep_tree_parsimonator.stderr
 #PBS -N 05_bootrep_parsimonator
